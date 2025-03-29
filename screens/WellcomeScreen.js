d082a7ff994fb3b3aa022e0f453632a888a3e824
Harjinder Singh <harjinders799@gmail.com>
2025-03-29 19:47:02 +0530

    welcome screen update

--- screens/WellcomeScreen.js
+++ screens/WellcomeScreen.js
@@ -1,31 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ActivityIndicator, Dimensions } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  Dimensions,
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getFontSize } from '../utils';
-import { createRStyle, useResponsiveMethods } from 'react-native-full-responsive';
+import {getFontSize} from '../utils';
+import {createRStyle, useResponsiveMethods} from 'react-native-full-responsive';
 
 const window = Dimensions.get('window');
 
-const WellcomeScreen = ({ navigation }) => {
-  const { rs, rw, rh } = useResponsiveMethods();
+const WellcomeScreen = ({navigation}) => {
+  const {rs, rw, rh} = useResponsiveMethods();
 
   const [loading, setLoading] = useState(false);
 
   const handlePress = async () => {
-    setLoading(true);  // Show loading indicator during the check
+    setLoading(true); // Show loading indicator during the check
     try {
       const userToken = await AsyncStorage.getItem('userToken');
       if (userToken) {
-        navigation.replace('HomeScreen');  // Navigate to Home if logged in
+        navigation.replace('HomeScreen'); // Navigate to Home if logged in
       } else {
-        navigation.replace('Login');  // Navigate to Login if not logged in
+        navigation.replace('Login'); // Navigate to Login if not logged in
       }
     } catch (error) {
       console.error('Error checking the session state.', error);
-      navigation.replace('Login');  // Fallback to Login on error
+      navigation.replace('Login'); // Fallback to Login on error
     } finally {
-      setLoading(false);  // Stop loading indicator
+      setLoading(false); // Stop loading indicator
     }
   };
 
@@ -33,12 +41,17 @@ const WellcomeScreen = ({ navigation }) => {
     <LinearGradient colors={['#d9d600', '#760075']} style={styles.gradient}>
       <View style={styles.container}>
         <Image source={require('../images/logo2.webp')} style={styles.logo} />
-        <Text style={styles.title}>Cultura Catalana immersiva</Text>
-        <TouchableOpacity style={styles.button} onPress={handlePress} disabled={loading}>
+        <Text style={[styles.title, {fontSize: rs(18)}]}>
+          Cultura Catalana immersiva
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handlePress}
+          disabled={loading}>
           {loading ? (
             <ActivityIndicator size="small" color="#fff" />
           ) : (
-            <Text style={styles.txt}>Entra</Text>
+            <Text style={[styles.txt, {fontSize: rs(15)}]}>Entra</Text>
           )}
         </TouchableOpacity>
       </View>
@@ -49,24 +62,27 @@ const WellcomeScreen = ({ navigation }) => {
 const styles = createRStyle({
   gradient: {
     flex: 1,
+    // justifyContent: 'center',
   },
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    // flex: 1 .
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     padding: '5rs',
-    width: '100%'
+    paddingTop: '55rs',
+    width: '100%',
+    height: window.height / 1.2,
   },
   logo: {
-    width: '50%',
+    width: '95%',
     height: window.height * 0.2,
     resizeMode: 'contain',
-    marginBottom: '5rs'
+    // marginBottom: '5rs',
   },
   title: {
     fontSize: '14rs',
     color: '#ffffff',
-    marginBottom: '5%'
+    // marginBottom: '5%',
   },
   button: {
     backgroundColor: '#5c10b2',
@@ -76,6 +92,6 @@ const styles = createRStyle({
     alignItems: 'center',
     width: '80%',
   },
-  txt:{ color: 'white',fontSize:'10rs' }
+  txt: {color: 'white', fontSize: '10rs'},
 });
 export default WellcomeScreen;
